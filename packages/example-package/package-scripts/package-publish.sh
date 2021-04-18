a66c6d179c5174e8e7ebb52815630989b97c8ae2
socks <somesocks@gmail.com>
2021-04-18 23:36:24 +0200

    - adding publish tools (for public packages)

---
+++ packages/example-package/package-scripts/package-publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# WARNING: this package is auto-generated from a template
+# do not try to make changes in here, they will be overwritten
+
+#
+# turn this on to debug script
+# set -x
+
+#
+# abort on error
+# https://sipb.mit.edu/doc/safe-shell/
+set -euf -o pipefail
+
+ROOT_DIR=.
+SRC_DIR=$ROOT_DIR/src
+DIST_DIR=$ROOT_DIR/dist
+
+SCRIPTS_DIR=$ROOT_DIR/package-scripts
+SOURCE_MAP=$ROOT_DIR/.sourcemap
+
+pnpm publish
