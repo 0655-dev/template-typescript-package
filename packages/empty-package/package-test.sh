43f22b9b509a9c8e76d22f71e8db4605a9e57aef
socks <somesocks@gmail.com>
2021-04-25 11:29:25 +0200

    - working on CI tests fix

--- packages/empty-package/package-test.sh
+++ packages/empty-package/package-test.sh
@@ -8,11 +8,11 @@ rm -rf ./dist && mkdir ./dist;
 
 cp ./package-bootstrap.sh ./dist/;
 
-# we need to 'trick' pnpm into thinking this is not a CI run, so that it doesn't freeze the lockfile for this test
 ( \
 	cd ./dist \
 	&& ./package-bootstrap.sh \
-	&& CI='' CONTINUOUS_INTEGRATION='' BUILD_NUMBER='' RUN_ID='' make test \
+	&& pnpm install --filter ./ --frozen-lockfile=false \
+	&& make test \
 );
 
 rm -rf ./dist;
